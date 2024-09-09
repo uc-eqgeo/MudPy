6b498333cd74654162ead1975d2dacb955075cbb
JackDMcGrath <44168380+JackDMcGrath@users.noreply.github.com>
2024-09-09 18:48:02 +1200

    Name data from columns

--- scripts/rupture2vtk.py
+++ scripts/rupture2vtk.py
@@ -4,6 +4,7 @@ from pyproj import Transformer
 from scipy.spatial import KDTree
 from glob import glob
 import os
+import pandas as pd
 
 
 mesh_folder = 'C:\\Users\\jmc753\\Work\\RSQSim\\Aotearoa\\fault_vtks'
@@ -15,19 +16,19 @@ vtk = meshio.read('C:\\Users\\jmc753\\Work\\RSQSim\\Aotearoa\\fault_vtks\\subduc
 rupture_dir = "Z:\\McGrath\\HikurangiFakeQuakes\\hikkerk3D\\output\\ruptures"
 
 n_ruptures = 5000
-inversion_name = 'start_rand'
+inversion_name = 'hires_deficit'
 rupture_list = glob(f'{rupture_dir}\\..\\{inversion_name}\\n{n_ruptures}*.inv')
 
 # Create interpolation object for mapping ruptures to the mesh
 transformer = Transformer.from_crs("epsg:4326", "epsg:2193")
 for rupture_file in rupture_list[::-1]:
     rupture_file = os.path.abspath(rupture_file)
-    rupture = np.loadtxt(rupture_file)
+    rupture = pd.read_csv(rupture_file, sep='\t', index_col=0).reset_index(drop=True)
 
-    patch_coords = np.zeros_like(rupture[:, :4])
-    patch_coords[:, 0] = np.arange(rupture.shape[0])
-    patch_coords[:, 2], patch_coords[:, 1] = transformer.transform(rupture[:, 2], rupture[:, 1])
-    patch_coords[:, 3] = rupture[:, 3] * -1000
+    patch_coords = np.zeros((rupture.shape[0], 4))
+    patch_coords[:, 0] = rupture.index.to_numpy()
+    patch_coords[:, 2], patch_coords[:, 1] = transformer.transform(rupture['lat'], rupture['lon'])
+    patch_coords[:, 3] = rupture['z(km)'] * -1000
 
     cells = vtk.cells[0].data
     n_cells = cells.shape[0]
@@ -48,11 +49,27 @@ for rupture_file in rupture_list[::-1]:
     _, nearest_indices = hikurangi_kd_tree.query(cell_centers)
 
     points = vtk.points
-    ss = rupture[nearest_indices, 8]
-    ds = rupture[nearest_indices, 9]
-    total = np.sqrt(ss**2 + ds**2)
 
-    rupture_mesh = meshio.Mesh(points=points, cells=[(element, cells)], cell_data={'ss': [ss], 'ds': [ds], 'total': [total]})
+    ss, ds = False, False
+    col_dict = {}
+
+    for col in rupture.columns:
+        if col in ['lat', 'lon', 'z(km)']:
+            continue
+        if len(np.unique(rupture[col])) > 1:  # Don't bother with constants
+            col_dict[col] = [rupture.loc[nearest_indices, col].values]
+            print(col, col_dict.keys())
+            if 'ss' in col:
+                print('ss:', col)
+                ss, ss_col = True, col
+            elif 'ds' in col:
+                print('ds:', col)
+                ds, ds_col = True, col
+
+    if all([ds, ss]):
+        col_dict['total'] = [np.sqrt(rupture[ss_col]**2 + rupture[ds_col]**2)]
+
+    rupture_mesh = meshio.Mesh(points=points, cells=[(element, cells)], cell_data=col_dict)
 
     outfile = f"{rupture_file.replace('scaling.', 'scaling+').split('.')[0].replace('scaling+', 'scaling.')}{suffix}.vtk"
     rupture_mesh.write(outfile, file_format="vtk")
