eff8c0aa459856f79f2409fb279d2637af9d1dcd
JackDMcGrath <44168380+JackDMcGrath@users.noreply.github.com>
2024-09-16 16:27:08 +1200

    Work in pandas

--- scripts/plot_mw_stats.py
+++ scripts/plot_mw_stats.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# %%
 """
 Created on Fri Aug  9 16:08:52 2024
 
@@ -9,7 +9,8 @@ from glob import glob
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import pandas as pd
+import os
 
 locking = True
 NZSHM = True
@@ -28,68 +29,106 @@ else:
 if force_Mw:
     tag += '_forceMw'
 
-# logs = glob(f"C:\\Users\\jmc753\\Work\\MudPy\\examples\\fakequakes\\3D\\hikkerk3D_test\\output\\ruptures\\hikkerk3D_{tag}*.log")
-logs = glob(f"Z:\\McGrath\\HikurangiFakeQuakes\\hikkerk3D\\output\\ruptures\\hikkerk3D_{tag}*.log")
-length = []
-width = []
-target = []
-actual = []
-lon = []
-lat = []
-z = []
-clon = []
-clat = []
-cz = []
-requested = 100  # Original request per mag bin
-for ix, log in enumerate(logs):
-    with open(log) as fid:
-        lines = fid.readlines()
-        for line in lines:
-            if 'length Lmax' in line:
-                length.append(float(line.split()[-2]))
-            if 'width Wmax' in line:
-                width.append(float(line.split()[-2]))
-            if 'Target magnitude' in line:
-                target.append(float(line.strip('\n').split()[-1]))
-            if 'Actual magnitude' in line:
-                actual.append(float(line.strip('\n').split()[-1]))
-            if 'Hypocenter (lon,lat,z[km])' in line:
-                lon_tmp, lat_tmp, z_tmp = line.strip('\n').split()[-1].split(',')
-                if float(lon_tmp.strip('()')) < 0:
-                    lon.append(float(lon_tmp.strip('()')) + 360)
-                else:
-                    lon.append(float(lon_tmp.strip('()')))
-                lat.append(float(lat_tmp.strip('()')))
-                z.append(float(z_tmp.strip('()')))
-            if 'Centroid (lon,lat,z[km])' in line:
-                lon_tmp, lat_tmp, z_tmp = line.strip('\n').split()[-1].split(',')
-                if float(lon_tmp.strip('()')) < 0:
-                    clon.append(float(lon_tmp.strip('()')) + 360)
-                else:
-                    clon.append(float(lon_tmp.strip('()')))
-                clat.append(float(lat_tmp.strip('()')))
-                cz.append(float(z_tmp.strip('()')))
+logs = glob(f"Z:\\McGrath\\HikurangiFakeQuakes\\hikkerk3D_hires\\output\\ruptures\\hikkerk3D_{tag}*.log")
+logs.sort()
+stats_csv = os.path.abspath(os.path.join(os.path.dirname(logs[0]), '..', 'rupture_stats.csv'))
 
-actual = np.array(actual)
-order = np.argsort(actual)
-length = np.array(length)
-width = np.array(width)
-lon = np.array(lon)
-lat = np.array(lat)
-z = np.array(z)
-clon = np.array(clon)
-clat = np.array(clat)
-cz = np.array(cz)
+remake_csv = False
+min_mw = 6
+max_mw = 10
+if os.path.exists(stats_csv) and not remake_csv:
+    mw_df = pd.read_csv(stats_csv)
+else:
+    length = []
+    width = []
+    target = []
+    actual = []
+    lon = []
+    lat = []
+    z = []
+    clon = []
+    clat = []
+    cz = []
+    requested = 100  # Original request per mag bin
+    for ix, log in enumerate(logs):
+        print(f"Processing {ix + 1}/{len(logs)}", end='\r')
+        if float(log.split('Mw')[1].split('_')[0].replace('-','.')) < min_mw:
+            continue
+        elif float(log.split('Mw')[1].split('_')[0].replace('-','.')) > max_mw:
+            continue
+        else:
+            target.append(float(log.split('Mw')[1].split('_')[0].replace('-','.')))
+        with open(log) as fid:
+            lines = fid.readlines()
+            data_lines = [lines[ix] for ix in [11, 12, 16, 17, 19]]
+            for line in data_lines:
+                if 'length Lmax' in line:
+                    length.append(float(line.split()[-2]))
+                if 'width Wmax' in line:
+                    width.append(float(line.split()[-2]))
+                if 'Actual magnitude' in line:
+                    actual.append(float(line.strip('\n').split()[-1]))
+                if 'Hypocenter (lon,lat,z[km])' in line:
+                    lon_tmp, lat_tmp, z_tmp = line.strip('\n').split()[-1].split(',')
+                    if float(lon_tmp.strip('()')) < 0:
+                        lon.append(float(lon_tmp.strip('()')) + 360)
+                    else:
+                        lon.append(float(lon_tmp.strip('()')))
+                    lat.append(float(lat_tmp.strip('()')))
+                    z.append(float(z_tmp.strip('()')))
+                if 'Centroid (lon,lat,z[km])' in line:
+                    lon_tmp, lat_tmp, z_tmp = line.strip('\n').split()[-1].split(',')
+                    if float(lon_tmp.strip('()')) < 0:
+                        clon.append(float(lon_tmp.strip('()')) + 360)
+                    else:
+                        clon.append(float(lon_tmp.strip('()')))
+                    clat.append(float(lat_tmp.strip('()')))
+                    cz.append(float(z_tmp.strip('()')))
 
-min_mw = np.floor(min(np.hstack([actual, target])) * 20) / 20
-max_mw = np.ceil(max(np.hstack([actual, target])) * 20) / 20
-sample_mw = np.arange(min_mw, max_mw, 0.01)
-target_arr = np.array(target)
-step_mw = np.min(np.diff(np.unique(target_arr)))
+    actual = np.array(actual)
+    order = np.argsort(actual)
+    length = np.array(length)
+    width = np.array(width)
+    lon = np.array(lon)
+    lat = np.array(lat)
+    z = np.array(z)
+    clon = np.array(clon)
+    clat = np.array(clat)
+    cz = np.array(cz)
 
+    target = np.array(target)
+
+    mw_dict = {'actual': actual,
+            'order': order,
+            'length': length,
+            'width': width,
+            'lon': lon,
+            'lat': lat,
+            'z': z,
+            'clon': clon,
+            'clat': clat,
+            'cz': cz,
+            'target': target}
+
+    mw_df = pd.DataFrame(mw_dict)
+    mw_df.to_csv(stats_csv, index=False)
+
+
+min_mw = np.floor(min(mw_df[['actual', 'target']].min()) * 20) / 20
+max_mw = np.ceil(max(mw_df[['actual', 'target']].max()) * 20) / 20
+step_mw = np.round(np.min(np.diff(np.unique(mw_df['target']))), 4)
+sample_mw = np.round(np.arange(min_mw, max_mw, step_mw), 4)
 # %%
+rupt_ids = np.array([[np.round(float(log.split('Mw')[1].split('_')[0].replace('-','.')), 4), int(log.split('_')[-1].split('.')[0])] for log in logs])
+requested = np.zeros_like(sample_mw)
+for ix, mag in enumerate(sample_mw):
+    if np.isin(rupt_ids[:, 0], mag).any():
+        requested[ix] = int(np.max(rupt_ids[np.isin(rupt_ids[:, 0], mag), 1])) + 1
+    else:
+        requested[ix] = 0
+# %%  Target vs Actual Scatter
 # plt.scatter(target, actual)
-sns.histplot(x=target, y=actual, binwidth=0.05, binrange=[sample_mw[0], sample_mw[-1]])
+sns.histplot(mw_df, x='target', y='actual', binwidth=0.05, binrange=[sample_mw[0], sample_mw[-1]])
 plt.plot(sample_mw, sample_mw, color='red')
 plt.xlabel('Target Mw')
 plt.ylabel('Actual Mw')
@@ -98,11 +137,8 @@ plt.xlim([6, 9.5])
 plt.ylim([6, 9.5])
 plt.show()
 
-# %%
-# plt.scatter(actual, np.log10(length * width), label='Actual')
-# plt.scatter(target, np.log10(length * width), s=25, label='Target')
-sns.histplot(x=actual, y=np.log10(length * width), binwidth=step_mw, cmap='viridis')
-# sns.histplot(x=target, y=np.log10(length * width), binwidth=step_mw, cmap='flare_r')
+# %% Histplot Actual vs Area
+sns.histplot(mw_df, x='actual', y=np.log10(mw_df['length'] * mw_df['width']), binwidth=step_mw, cmap='viridis')
 plt.plot(sample_mw, sample_mw - 4.0, color='red', linestyle=':', lw=1, label='NZ NSHM relation')
 plt.plot(sample_mw, sample_mw - 3.6, color='red', linestyle=':', lw=0.5)
 plt.plot(sample_mw, sample_mw - 4.1, color='red', linestyle=':', lw=0.5)
@@ -114,8 +150,8 @@ plt.xlim([6, 9.5])
 plt.ylim([1.5, 5.5])
 plt.show()
 
-# %%
-sns.scatterplot(x=actual, y=np.log10(length * width), hue=target, size=5, markers=False)
+# %% Scatter Actual vs Area
+sns.scatterplot(mw_df, x='actual', y=np.log10(mw_df['length'] * mw_df['width']), hue='target', s=10, markers=False)
 plt.plot(sample_mw, sample_mw - 4.0, color='red', linestyle=':', lw=1, label='NZ NSHM relation')
 plt.plot(sample_mw, sample_mw - 3.6, color='red', linestyle=':', lw=0.5)
 plt.plot(sample_mw, sample_mw - 4.1, color='red', linestyle=':', lw=0.5)
@@ -128,26 +164,31 @@ plt.ylim([1.5, 5.5])
 plt.show()
 
 
-# %%
-sns.histplot(actual, binwidth=step_mw, binrange=[sample_mw[0], sample_mw[-1]])
-plt.axhline(requested)
-plt.axvline(min(target))
-plt.axvline(max(target))
+# %% Histplot Target and Actual Mw
+sns.histplot(mw_df['target'], binwidth=step_mw-1e-4, binrange=[sample_mw[0], sample_mw[-1]], label='Target')
+sns.histplot(mw_df['actual'], binwidth=step_mw-1e-4, binrange=[sample_mw[0], sample_mw[-1]], label='Actual')
+plt.plot(sample_mw, requested, color='red')
+#plt.axhline(requested)
+plt.axvline(mw_df['target'].min())
+plt.axvline(mw_df['target'].max())
+plt.legend()
 plt.show()
 
-# %%
-sns.histplot(x=lon, y=lat, binwidth=0.1, cmap='flare_r')
+# %% Hisplot Hypocenter
+sns.histplot(mw_df, x='lon', y='lat', binwidth=0.1, cmap='flare_r')
 plt.title('Hypocentre')
 plt.show()
 
-# %%
-mw_bin_size = 1
-Mw_round = np.round(actual * (1 / mw_bin_size), 0) / (1 / mw_bin_size)
+# %% Incremental Hypocenters
+mw_bin_size = 1.
+Mw_round = np.round(mw_df['actual'] * (1 / mw_bin_size), 0) / (1 / mw_bin_size)
 
 for mag in np.unique(Mw_round):
     ix = np.where(Mw_round == mag)[0]
-    sns.histplot(x=lon[ix], y=lat[ix], binwidth=0.1, cmap='flare_r')
+    sns.histplot(x=mw_df.loc[mw_df['actual'].round() == mag, 'lon'], y=mw_df.loc[mw_df['actual'].round() == mag, 'lat'], binwidth=0.1, cmap='flare_r')
     plt.title(f'Hypocentre {mag}Mw')
     plt.xlim([172, 186])
     plt.ylim([-43, -23])
     plt.show()
+
+# %%
