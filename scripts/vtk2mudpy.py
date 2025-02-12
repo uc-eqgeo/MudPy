c56b77c8a2c1409e91dd00028bd1ae77336aeee0
JackDMcGrath <jdmcgrath95@gmail.com>
2025-02-13 11:00:39 +1300

    Correct to deal with vtks of different resolution

--- scripts/vtk2mudpy.py
+++ scripts/vtk2mudpy.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import math
 import os
 
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 def vector_to_bearing(vector):
     # Calculate the angle in radians
     angle_radians = math.atan2(vector[1], vector[0])
@@ -20,15 +22,14 @@ def vector_to_bearing(vector):
 
 data_folder = '../data/'
 mesh_folder = data_folder
-vtk_file = 'fq_hik_kerm_adjusted_lock.vtk'
+vtk_file = 'hik_kerm_adjusted_plate70.vtk'  # VTK file that contains the slip deficit model
 
 sub_type = 'hk'
-outname = 'hk_locked'
+outname = 'plate70'
 
-data = np.load(f'{mesh_folder}\\data\\{sub_type}_tile_outlines.npy')
+data = np.load(os.path.join(mesh_folder,f"{sub_type}_tile_outlines.npy"))  # .npy file that contains the coordinates of the subduction zone tiles
 
 vtk = meshio.read(os.path.join(data_folder, vtk_file))
-
 
 patch_centers = np.mean(data, axis=1)
 dimensions = np.max(data, axis=1) - np.min(data, axis=1)
@@ -36,6 +37,7 @@ dimensions = np.max(data, axis=1) - np.min(data, axis=1)
 length = 5000
 width = 5000
 
+# Read in the mesh data, and find the center point of each cell
 cells = vtk.cells[0].data
 n_cells = cells.shape[0]
 cell_centers = np.zeros((n_cells, 3))
@@ -47,6 +49,7 @@ for ii in range(n_cells):
         p1, p2, p3, p4 = vtk.cells[0].data[ii, :]
         cell_centers[ii, :] = np.mean(np.vstack([vtk.points[p1, :], vtk.points[p2, :], vtk.points[p3, :], vtk.points[p4, :]]), axis=0)
 
+# Find the nearest cell to each patch center using KDTrees
 mesh_kd_tree = KDTree(cell_centers)
 _, nearest_indices = mesh_kd_tree.query(patch_centers)
 
@@ -62,13 +65,16 @@ else:
 
 ll = np.zeros((patch_centers.shape[0], 2))
 
-write_to_fault = False
-write_to_slip = True
+write_to_fault = False  # Write out the patch geometry to .fault file
+write_to_slip = True  # Write out slip deficit to .slip file that matches .fault
+write_vtk_to_slip = False  # Write out slip deficit to .slip file that matches the input VTK file
 
+# Create a transformer object to convert between coordinate systems (NZTM to WGS84)
 transformer = Transformer.from_crs("epsg:2193", "epsg:4326")
 
 if write_to_fault:
-    out_file = os.path.join({data_folder}, f"{outname}.fault")
+    print("Write out fault geometry file")
+    out_file = os.path.join(data_folder, f"{sub_type}_{outname}.fault")
     with open(out_file, 'w') as fid:
         fid.write('# No.\tlon\tlat\tz\tstrike\tdip\ttyp\trt\tlength\twidth\trake\n')
         fid.write('#\t(deg)\t(deg)\t(km)\t(deg)\t(deg)\t()\t()\t(m)\t(m)\t(deg)\n')
@@ -81,7 +87,27 @@ if write_to_fault:
             ll[ix, :] = np.array([lon, lat])
 
 if write_to_slip:
-    out_file = os.path.join({data_folder}, f"{outname}.slip")
+    out_file = os.path.join(data_folder, f"{sub_type}_{outname}.slip")
+    print("Write out slip file")
+    deficits = []
+    with open(out_file, 'w') as fid:
+        fid.write('#No\tlon\tlat\tz(km)\tstrike\tdip\trise\tdura\tss-deficit(mm/yr)\tds-deficit(mm/yr)\trupt_time\trigid\tvel\n')
+        for ix, cell in enumerate(patch_centers):
+            lat, lon = transformer.transform(cell[1], cell[0])
+            z = cell[2]
+            deficit = vtk.cell_data['slip'][0][nearest_indices[ix]]
+            if deficit == 0:
+                deficit = 0.001
+            deficits.append(deficit)
+            fid.write(f"{ix+1}\t{lon:.6f}\t{lat:.6f}\t{-z:.6f}\t{vector_to_bearing(strikes[ix]):.2f}\t{dips[ix]}\t0\t0\t0\t{deficit:.6f}\t0\t0\t0\n")
+
+            if lon < 0:
+                lon += 360
+            ll[ix, :] = np.array([lon, lat])
+
+if write_vtk_to_slip:
+    out_file = os.path.join(data_folder, vtk_file.replace('.vtk', '.slip'))
+    print("Write out vtk to slip file (as a bonus)")
     with open(out_file, 'w') as fid:
         fid.write('#No\tlon\tlat\tz(km)\tstrike\tdip\trise\tdura\tss-deficit(mm/yr)\tds-deficit(mm/yr)\trupt_time\trigid\tvel\n')
         for ix, cell in enumerate(cell_centers):
@@ -92,14 +118,7 @@ if write_to_slip:
                 deficit = 0.001
             fid.write(f"{ix+1}\t{lon:.6f}\t{lat:.6f}\t{-z:.6f}\t{vector_to_bearing(strikes[ix]):.2f}\t{dips[ix]}\t0\t0\t0\t{deficit:.6f}\t0\t0\t0\n")
 
-            if lon < 0:
-                lon += 360
-            ll[ix, :] = np.array([lon, lat])
-
-plt.scatter(ll[:,0], ll[:,1], c=dips)
-plt.colorbar()
-plt.show()
-
-plt.scatter(patch_centers[:, 0], patch_centers[:, 1], c=dips)
-plt.colorbar()
-plt.show()
+for var in [rakes, deficits]:
+    plt.scatter(patch_centers[:, 0], patch_centers[:, 1], c=var)
+    plt.colorbar()
+    plt.show()
