b3ec0ad6ca300aeb8da88004dcb55b76c91628f4
JackDMcGrath <44168380+JackDMcGrath@users.noreply.github.com>
2025-04-15 12:24:27 +1200

    Allow to read from premade catalogue

--- scripts/make_rupture_csv_parallel.py
+++ scripts/make_rupture_csv_parallel.py
@@ -77,34 +77,92 @@ if rake:
     rake_tag = '_rake'
 else:
     rake_tag = ''
+min_Mw = 6.5
+max_Mw = 9.0
 
+rupt_name = rupt_name.replace('*.rupt','').strip('.')
+total_ruptures = len(rupture_list)  # Total number of prepared ruptures
+rupture_list = [rupture_list[ix] for ix in np.random.permutation(total_ruptures) if float(rupture_list[ix].split('.Mw')[-1].split('_')[0].replace('-', '.')) < max_Mw and float(rupture_list[ix].split('.Mw')[-1].split('_')[0].replace('-', '.')) >= min_Mw]
 n_ruptures = len(rupture_list)  # Number of ruptures
-rupture_list = [rupture_list[ix] for ix in np.random.permutation(n_ruptures)]
-print(f"{n_ruptures} found")
-if n_ruptures != 50000:
+print(f"{total_ruptures} found, {n_ruptures} between {min_Mw}-{max_Mw}Mw")
+if n_ruptures != 50000 and max_Mw < 9.5 and min_Mw > 6.5:
     raise Exception("Incorrect number of ruptures")
+
+compiled_csv = os.path.abspath(os.path.join(rupture_dir, "..", f'{rupt_name}_df_n{n_ruptures}{rake_tag}.csv'))
+check_prepared = False
+if rake == False and os.path.exists(compiled_csv.replace('.csv', '_rake.csv')):
+    print(f"Checking pre-prepared rupture order from {os.path.basename(compiled_csv.replace('.csv', '_rake.csv'))}")
+    premade_df_file = compiled_csv.replace('.csv', '_rake.csv')
+    check_prepared = True
+elif rake == True and os.path.exists(compiled_csv.replace('_rake.csv', '.csv')):
+    print(f"Checking pre-prepared rupture order from {os.path.basename(compiled_csv.replace('_rake.csv', '.csv'))}")
+    premade_df_file = compiled_csv.replace('_rake.csv', '.csv')
+    check_prepared = True
+
+if check_prepared:
+    print('\tLoading rupt_ids...')
+    premade_df = pd.read_csv(premade_df_file, usecols=['rupt_id'])
+    print('\tExtracting rupt_ids...')
+    premade_rupture_list = [f"{rupture_dir}{rupt_name.replace('*.rupt','').strip('.')}.Mw{rupt_id}.rupt" for rupt_id in premade_df.rupt_id]
+    print('\tCross checking rupt_ids...')
+    premade_rupture_set = set(premade_rupture_list)
+    check_rupts = [True if rupture in premade_rupture_set else False for rupture in rupture_list]
+    del premade_df
+    if all(check_rupts):
+        print("All ruptures are in the pre-prepared df.")
+        rupture_list = premade_rupture_list           
+    else:
+        for rupture in rupture_list:
+            if rupture not in premade_rupture_set:
+                print(f"Missing rupture: {rupture}")
+        raise Exception("Some requested ruptures are not in the pre-prepared list.")
+
 deficit = np.genfromtxt(rupture_list[0])
 n_patches = deficit.shape[0]  # Number of patches
 columns = ['mw', 'target_mw'] + [patch for patch in range(n_patches)]
 rupture_df = pd.DataFrame(columns=columns)
+rupture_df.index.name = 'rupt_id'
 
 block_size = 1000
 block_starts = np.arange(0, n_ruptures, block_size)
 block_ends = block_starts + block_size
 num_threads_plot = 10
 
-rupt_name = rupt_name.replace('*.rupt','').strip('.')
 if __name__ == '__main__':
-    with Pool(processes=num_threads_plot) as block_pool:
-        block_pool.starmap(write_block, [(rupt_name, rupture_list[start:end], end, columns, rake) for start, end in zip(block_starts, block_ends)])
+    if not rake and check_prepared:
+        print(f"Extracting from {os.path.basename(premade_df_file)}")
+        try:
+            print("\tLoading ruptures w/ rakes...")
+            rake_df = pd.read_csv(premade_df_file)
+            print(f"\tIdentifying rakes...")
+            rake_ix = [ix for ix, rupt_id in enumerate(rake_df['rupt_id'].values) if 'rake' in rupt_id]
+            print(f"\tDropping rakes...")
+            rake_df.drop(rake_ix, inplace=True)
+            total_ruptures = rake_df.shape[0]
+            print(f"\tWriting {total_ruptures} ruptures...", end='\r')
+            rake_df.to_csv(compiled_csv, index=False)
+        except:
+            print(f"\tRetry in blocks")
+            rupture_df.to_csv(compiled_csv)
+            total_ruptures = rupture_df.shape[0]
+            for block_start in block_starts:
+                print(f"Merging block {block_start}...", end='\r')
+                rake_df = pd.read_csv(premade_df_file, skiprows=block_start * 2, nrows=block_size * 2, header=None)
+                rake_df = rake_df.iloc[[ix for ix, row in rake_df.iterrows() if 'rake' not in row[0]]].reset_index(drop=True)
+                rake_df.to_csv(compiled_csv, mode='a', header=False, index=False)
+                total_ruptures += rake_df.shape[0]
 
-    rupture_df.index.name = 'rupt_id'
-    rupture_df.to_csv(os.path.abspath(os.path.join(rupture_dir, "..", f'{rupt_name}_df_n{n_ruptures}{rake_tag}.csv')))
+    else:
+        with Pool(processes=num_threads_plot) as block_pool:
+            block_pool.starmap(write_block, [(rupt_name, rupture_list[start:end], end, columns, rake) for start, end in zip(block_starts, block_ends)])
 
-    total_ruptures = rupture_df.shape[0]
-    for block in block_starts + block_size:
-        print(f"Merging block {block}...", end='\r')
-        rupture_df = pd.read_csv(os.path.abspath(os.path.join(rupture_dir, "..", f'{rupt_name}_df_n{block}{rake_tag}_block.csv')))
-        rupture_df.to_csv(os.path.abspath(os.path.join(rupture_dir, "..", f'{rupt_name}_df_n{n_ruptures}{rake_tag}.csv')), mode='a', header=False, index=False)
-        total_ruptures += rupture_df.shape[0]
-    print(f"\nCompleted {total_ruptures} ruptures! :) ", os.path.abspath(os.path.join(rupture_dir, "..", f'{rupt_name}_df_n{n_ruptures}{rake_tag}.csv')))
+        rupture_df.to_csv(compiled_csv)
+
+        total_ruptures = rupture_df.shape[0]
+        for block in block_starts + block_size:
+            print(f"Merging block {block}...", end='\r')
+            rupture_df = pd.read_csv(compiled_csv.replace(f"n{n_ruptures}", f"n{block}").replace(".csv", "_block.csv"))
+            rupture_df.to_csv(compiled_csv, mode='a', header=False, index=False)
+            total_ruptures += rupture_df.shape[0]
+
+    print(f"\nCompleted {total_ruptures} ruptures! :) ", compiled_csv)
